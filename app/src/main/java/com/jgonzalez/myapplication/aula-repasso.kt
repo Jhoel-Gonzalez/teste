b82ae4426f8fd4ce3ea03b941532e12b9475d735
Jhoel Gonzalez <jhoel834@gmail.com>
2024-11-09 15:24:36 -0400

    Estructura base do projeto

---
+++ app/src/main/java/com/jgonzalez/myapplication/aula-repasso.kt
@@ -0,0 +1,145 @@
+//package com.jgonzalez.myapplication
+//
+//fun main() {
+//
+//    println("Digite a nota 1")
+//    val nota1:Int = readln().toInt()
+//    println("Digite a nota 2")
+//    val nota2:Int = readln().toInt()
+//    println("Digite a nota 3")
+//    val nota3:Int = readln().toInt()
+//    println("Digite a nota 4")
+//    val nota4:Int = readln().toInt()
+//
+//    val media: Int = (nota1 + nota2 + nota3 + nota4) / 4
+//
+//    if(media >= 7) {
+//        println("Media de: $media")
+//        println("Aluno Aprovado")
+//    } else {
+//        println("Media de: $media")
+//        println("Aluno em recuperação")
+//
+//        println("Digite a nota de recuperação")
+//        val notaRecuperação: Int = readln().toInt()
+//
+//        val mediaFinal: Int = (media + notaRecuperação) / 2
+//
+//        if (mediaFinal >= 7 ){
+//            println("Media Final de: $mediaFinal")
+//            println("Aluno Aprovado")
+//
+//        } else {
+//            println("Media Final de: $mediaFinal")
+//            println("Aluno Reprovado")
+//        }
+//    }
+//}
+
+
+//Classe Responsável por armazenar informações sobre uma pessoaclass Pessoa(
+////    var nome: String,
+////    var idade: Int,
+////    var cidade: String,
+////    var tipoSanguineo: Char,
+////    var falaIngles: Boolean,
+////    var falaAlemao: Boolean,
+////    var altura: Double
+////)
+////fun main() {
+////    // Informações sobre uma pessoa
+////    val pessoa1 = Pessoa(
+////        nome = "Filipe",
+////        idade = 28,
+////        cidade = "Rio de Janeiro",
+////        tipoSanguineo = 'A',
+////        falaIngles = true,
+////        falaAlemao = false,
+////        altura = 1.89
+////    )
+////
+////    val pessoa2 = Pessoa(
+////        nome = "Maria",
+////        idade = 29,
+////        cidade = "Salvador",
+////        tipoSanguineo = 'B',
+////        falaIngles = false,
+////        falaAlemao = true,
+////        altura = 1.85
+////    )
+////
+//////   println(pessoa1.nome)
+//////   println(pessoa1.cidade)
+//////   println(pessoa1.tipoSanguineo)
+//////   println(pessoa1.falaIngles)
+//////   println(pessoa1.falaAlemao)
+//////   println(pessoa1.altura)
+////
+//////   println()
+//////
+////    println(pessoa2.nome)
+////    println(pessoa2.cidade)
+////    println(pessoa2.tipoSanguineo)
+////    println(pessoa2.falaIngles)
+////    println(pessoa2.falaAlemao)
+////    println(pessoa2.altura)
+////}
+//
+
+
+//package introducaoLogicaProgramacao.aula4
+//
+//fun main() {
+//    val chamadaDaTurmaA: Array<String> = arrayOf(
+//        "Aline",
+//        "Bruno",
+//        "Carol",
+//        "Diego",
+//        "Eliana",
+//        "Filipe"
+//    )
+//
+//    // Imprimir o nome do primeiro aluno
+//    println(chamadaDaTurmaA[0])
+//    println()
+//
+//    // Imprimir o nome de todos os alunos
+//    for(nome in chamadaDaTurmaA){
+//        println(nome)
+//    }
+//    println()
+//
+//    // Juntar as turmas A e B
+//    val chamadaTurmaB : Array<String> = arrayOf(
+//        "Amanda",
+//        "Breno",
+//        "Camila",
+//        "Diogo"
+//    )
+//
+//    // Imprimir todos os alunos da nova turma formada por A + B
+//    val novaTurma = chamadaDaTurmaA + chamadaTurmaB
+//
+//    println("Nova turma")
+//    println()
+//    for (nome in novaTurma){
+//        println(nome)
+//    }
+//}
+
+
+fun main() {
+
+//    val mapa = mutableMapOf("chave1" to "valor1", "chave2" to "valor2")
+//    mapa["chave3"] = "valor3"
+//
+//    println(mapa)
+
+//    val conjunto = mutableSetOf(1,2,3,3)
+//    conjunto.add(4)
+//
+//    println(conjunto)
+
+
+}
+
